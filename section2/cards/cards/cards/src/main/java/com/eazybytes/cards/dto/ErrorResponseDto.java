c9e52aba6190db2612be6ddc73539bd8f9735436
Administrator <edisonvaz009@gmail.com>
2025-04-12 11:37:51 +0530

    added loan files

---
+++ section2/cards/cards/cards/src/main/java/com/eazybytes/cards/dto/ErrorResponseDto.java
@@ -0,0 +1,12 @@
+package com.eazybytes.cards.dto;
+
+public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private String errorCode;
+
+    private String errorMessage;
+
+    private String errorTime;
+}
---
+++ section2/cards/cards/cards/src/main/java/com/eazybytes/cards/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.eazybytes.cards.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+
+    private String statusMsg;
+
+
+}
